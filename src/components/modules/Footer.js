9a8e87e49007296c82466ecfafc88131c9d6b016
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-07-30 18:57:26 +0330

    add Subscribe to the newsletter

--- src/components/modules/Footer.js
+++ src/components/modules/Footer.js
@@ -17,8 +17,20 @@ const footerListData = [
 
 function Footer() {
   return (
-    <div className="bg-[#FAFAFA] w-full">
-      <div className="px-6 py-2.5 block md:flex w-full md:justify-evenly">
+    <div className="bg-[#FAFAFA] w-full px-6 py-6 grid">
+      <div>
+        <p className="font-semibold mb-2">عضویت در خبرنامه</p>
+        <div className="bg-white rounded-[7px] w-full h-[45px] flex justify-between items-center px-3 my-4">
+          <input
+            placeholder="ایمیل خود را وارد کنید"
+            className="text-[13px] w-full h-full  focus:outline-none"
+          />
+          <button className="w-[85px] h-[33px] rounded-[7px] text-[#FF510C] font-semibold bg-[#FF510C21] text-[15px] ">
+            عضویت
+          </button>
+        </div>
+      </div>
+      <div className=" py-2.5 block md:flex w-full md:justify-evenly">
         {footerListData.map((items) => (
           <FooterList id={items.id} name={items.name} item={items.item} />
         ))}
