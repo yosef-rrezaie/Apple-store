d15d761aa515f207ba1e19a4ccb402b1ea8de86d
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-07-30 21:10:44 +0330

    complete responsive footer

--- src/components/modules/Footer.js
+++ src/components/modules/Footer.js
@@ -1,26 +1,13 @@
 import Image from "next/image";
 import FooterList from "./FooterList";
+import { footerListData } from "@/utils/FooterListData";
 
-const footerListData = [
-  { id: 1, name: "فروشگاه", item: ["مک", "آیفون", "آیپد", "اپل واچ"] },
-  { id: 2, name: "لوازم جانبی", item: ["ایرپاد", "هوم پاد", "ایرتگ"] },
-  {
-    id: 3,
-    name: "دسترسی سریع",
-    item: ["حساب کاربری", "سبد خرید", "سبد خرید", "فروشگاه"],
-  },
-  {
-    id: 4,
-    name: "ارتباط با ما",
-    item: ["تماس با ما", "درباره ما", "نقشه سایت"],
-  },
-];
 
 function Footer() {
   return (
-    <div className="bg-[#FAFAFA] w-full px-6 py-6 grid">
-      <div>
-        <p className="font-semibold mb-2">عضویت در خبرنامه</p>
+    <div className="bg-[#FAFAFA] w-full px-6 py-6 md:grid md:grid-cols-3 md:pt-13">
+      <div className="md:col-start-2 md:col-end-3 md:w-[90%]">
+        <p className="font-semibold mb-2 md:text-[18px]">عضویت در خبرنامه</p>
         <div className="bg-white rounded-[7px] w-full h-[45px] flex justify-between items-center px-3 my-4">
           <input
             placeholder="ایمیل خود را وارد کنید"
@@ -31,45 +18,54 @@ function Footer() {
           </button>
         </div>
       </div>
-      <div className=" py-2.5 block md:flex w-full md:justify-evenly md:row-start-1 md:row-end-3">
+      <div className=" py-2.5 block md:flex w-full md:justify-evenly md:row-start-1 md:row-end-3 md:col-span-3">
         {footerListData.map((items) => (
           <FooterList id={items.id} name={items.name} item={items.item} />
         ))}
       </div>
-      <div className="bg-white rounded-2xl flex justify-between items-center p-4 mb-5">
-        <Image
-          src="/svg/instagram.svg"
-          width="1000"
-          height="1000"
-          className="w-4 h-4"
-          alt=""
-        />
-        <Image
-          src="/svg/whatsapp.svg"
-          width="1000"
-          height="1000"
-          className="w-4 h-4"
-          alt=""
-        />
-        <Image
-          src="/svg/twitter.svg"
-          width="1000"
-          height="1000"
-          className="w-4 h-4"
-          alt=""
-        />
+      <div
+        className="bg-white rounded-2xl flex justify-between items-center p-4 mb-5 
+      md:bg-[#fafafa] md:justify-center md:gap-x-4"
+      >
+        <div className="md:bg-white md:p-4 md:rounded-[10px]">
+          <Image
+            src="/svg/instagram.svg"
+            width="1000"
+            height="1000"
+            className="w-4 h-4 md:w-6.5 md:h-6.5"
+            alt=""
+          />
+        </div>
+        <div className="md:bg-white md:p-4 md:rounded-[10px]">
+          <Image
+            src="/svg/whatsapp.svg"
+            width="1000"
+            height="1000"
+            className="w-4 h-4 md:w-6.5 md:h-6.5"
+            alt=""
+          />
+        </div>
+        <div className="md:bg-white md:p-4 md:rounded-[10px]">
+          <Image
+            src="/svg/twitter.svg"
+            width="1000"
+            height="1000"
+            className="w-4 h-4 md:w-6.5 md:h-6.5"
+            alt=""
+          />
+        </div>
       </div>
-      <div className="mx-auto mb-5">
+      <div className="flex justify-center mb-5 md:col-start-1 md:col-end-2 md:row-start-3">
         <Image
           src="/images/enamad.webp"
           width="1000"
           height="1000"
-          className="w-15 h-21"
+          className="w-15 h-21 md:w-24 md:h-33"
         />
       </div>
-      <div className="text-[10px] w-full  pt-2 flex justify-center ">
-        <p className="">
-          تمامی حقوق برای {" "}
+      <div className="text-[10px]   pt-2 flex justify-center md : col-span-3  md:w-full  ">
+        <p className="md:text-[18px]">
+          تمامی حقوق برای{" "}
           <span className="text-[#FF510C]">وبسایت اپل استور </span> محفوظ است
         </p>
       </div>
