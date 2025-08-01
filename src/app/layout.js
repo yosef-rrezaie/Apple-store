5d21a21ae52e07de4acb48270be20af2029adcd9
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-08-01 16:39:06 +0330

    some changes in main layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,10 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className="font-sans">
-        <div className="bg-primary h-13 md:h-[70px] md:text-base flex justify-center items-center text-[13px] font-semibold text-white">
+        {/* <div className="bg-primary h-13 md:h-[70px] md:text-base flex justify-center items-center text-[13px] font-semibold text-white">
           <p>تا ۷۰٪ تخفیف برای لوازم جانبی اورجینال آیفون</p>
-        </div>
-        <Header/>
+        </div> */}
+        {/* <Header/> */}
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Header from "../components/layout/Header";
 import AdBanner from "@/components/modules/AdBanner";
 import AdvertiseBanner from "@/components/modules/AdvertiseBanner";
 import AppleWatch from "@/components/modules/AppleWatch";
@@ -11,6 +12,10 @@ import Footer from "@/components/modules/Footer";
 function Home() {
   return (
     <>
+      <div className="bg-primary h-13 md:h-[70px] md:text-base flex justify-center items-center text-[13px] font-semibold text-white">
+        <p>تا ۷۰٪ تخفیف برای لوازم جانبی اورجینال آیفون</p>
+      </div>
+      <Header />
       <Categories />
       <Banner />
       <CountDown />
