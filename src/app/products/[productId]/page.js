3dfca6b5e692e22a0d0e49c8c1650d634a5eb992
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-08-04 18:56:15 +0330

    some changes in productspage

--- src/app/products/[productId]/page.js
+++ src/app/products/[productId]/page.js
@@ -1,72 +1,83 @@
 import Image from "next/image";
 import { GrAd } from "react-icons/gr";
-import { FaCartShopping } from "react-icons/fa6";
 import { LuShoppingCart } from "react-icons/lu";
 import { FaStore } from "react-icons/fa";
-
 import { sp } from "@/utils/replaceNumber";
 
 async function ProductPage(req) {
-  const x = await req.params;
-  console.log(x);
   return (
-    <div className=" border bg-white shadow border-[#eb8d68] mx-6 mt-10 rounded-[13px] px-6 py-5">
-      <div className="flex justify-center relative">
+    <div className="border bg-white shadow-md border-[#eb8d68] mx-6 mt-10 rounded-[13px] px-8 py-6 ">
+      <div className="flex justify-center mb-6 ">
         <Image
           src="/images/apple-watch.webp"
           width="1000"
           height="1000"
-          className="w-40 h-40"
+          className="rounded-xl shadow-md w-50 md:w-60"
+          alt=" appleWatch"
         />
       </div>
-      <div className="flex mt-3">
-        <p className="text-[#FF510C]">نام کالا : </p>
-        <p className="mr-2 font-medium">اپل واچ سری Z</p>
+
+      <div className="text-center mb-4">
+        <h2 className="text-2xl font-bold text-gray-800">
+          سامسونگ گلکسی S25 اولترا
+        </h2>
+        <p className="text-sm text-gray-500 mt-1">دسته‌بندی: گوشی هوشمند</p>
       </div>
-      <div className="flex mt-3">
-        <p className="text-[#FF510C]"> دسته کالا : </p>
-        <p className="mr-2 font-medium">اپل واچ</p>
-      </div>
-      <div className="mt-3">
-        <p className="text-[#FF510C]">ویژگی های کالا : </p>
-        <div className="flex mt-3">
-          <GrAd className="" />
-          <p className="mr-2 font-medium">قابلیت شارژ دهی در 3 ساعت</p>
-        </div>
-        <div className="flex mt-3">
-          <GrAd className="" />
-          <p className="mr-2 font-medium">ضد آب و مقاومت در برابر ضربه</p>
-        </div>
-        <div className="flex mt-3">
-          <GrAd className="" />
-          <p className="mr-2 font-medium">قابلیت اتصال به اینترنت</p>
-        </div>
-      </div>
-      <div className="flex mt-3 flex-col">
-        <p className="text-[#FF510C]"> توضیحات : </p>
-        <p className="font-medium mt-3 text-justify">
-          این اپل واچ ساخت کشور تایوان است و در حال حاضر تنها نسخه سری Z تولید
-          می شود و ساخت بقیه نسخه ها متوقف شده است
-        </p>
-      </div>
-      <div className="mt-3 flex">
-        <FaStore className="text-red-500" />
-        <p className="mr-2 font-medium">فروشگاه شایان</p>
-      </div>
-      <div className="mt-3 flex justify-between ">
-        <p className="text-[#FF510C]">قیمت : </p>
-        <div className="flex flex-col items-baseline text-[17px] ">
-          <p className="text-red-700 font-semibold">{sp(55000000)}</p>
-          <div className="flex flex-row gap-x-2 justify-between items-center">
-            <p>{sp(23)}%</p>
-            <p className="line-through">{sp(56000000)}</p>
+
+      <div className="bg-gray-50 border border-dashed border-orange-200 p-4 rounded-xl mb-5">
+        <p className="text-[#FF510C] font-semibold mb-3">ویژگی‌ها:</p>
+        <div className="space-y-3">
+          <div className="flex items-center">
+            <GrAd className="text-orange-500 text-lg" />
+            <p className="mr-2 font-medium">
+              باتری 5000 میلی آمپر با شارژ سریع
+            </p>
+          </div>
+          <div className="flex items-center">
+            <GrAd className="text-orange-500 text-lg" />
+            <p className="mr-2 font-medium">دوربین اصلی 108 مگاپیکسل</p>
+          </div>
+          <div className="flex items-center">
+            <GrAd className="text-orange-500 text-lg" />
+            <p className="mr-2 font-medium">نمایشگر AMOLED با نرخ 120Hz</p>
+          </div>
+          <div className="flex items-center">
+            <GrAd className="text-orange-500 text-lg" />
+            <p className="mr-2 font-medium">ضد آب + بدنه گوریلا گلس</p>
           </div>
         </div>
       </div>
-      <div className="bg-[#FF510C] rounded-2xl p-2 mt-4 flex justify-center items-center">
-        <button className="flex text-white font-medium items-center justify-center ">
-          <LuShoppingCart className="text-[17px]" />
-          <p className="mr-2">اضافه کردن به سبد خرید</p>
+
+      <div className="mb-6">
+        <p className="text-[#FF510C] font-semibold mb-2">توضیحات:</p>
+        <p className="text-justify text-gray-700 leading-relaxed font-medium">
+          گلکسی S25 اولترا یکی از پرچم‌داران سامسونگ در سال جدید است که با طراحی
+          مدرن، عملکرد فوق‌العاده، و ویژگی‌های پیشرفته به بازار عرضه شده. این
+          گوشی مناسب گیمرها، عکاسان موبایلی و کاربران حرفه‌ای است.
+        </p>
+      </div>
+
+      <div className="flex items-center mb-4">
+        <FaStore className="text-red-500 text-xl" />
+        <p className="mr-2 font-semibold text-gray-800">فروشگاه دیجیتال‌پلاس</p>
+      </div>
+
+      <div className="flex flex-col md:flex-row-reverse md:items-center items-end justify-between mb-6 border-t border-gray-200 pt-4">
+        <div className=" flex flex-col items-end mb-8 md:mb-13">
+          <p className="text-red-600 text-xl font-bold">{sp(42900000)} تومان</p>
+          <div className="flex items-center gap-2 mt-1">
+            <span className="text-sm text-white bg-red-500 px-2 py-0.5 rounded-full">
+              ٪{sp(15)} تخفیف
+            </span>
+            <p className="line-through text-gray-400 text-sm">
+              {sp(50500000)} تومان
+            </p>
+          </div>
+        </div>
+        <button className="bg-[#FF510C] hover:bg-orange-600 text-white px-5 py-2 rounded-xl flex items-center justify-center 
+        transition-all w-full md:w-max">
+          <LuShoppingCart className="text-white text-lg" />
+          <span className="mr-2 font-medium">افزودن به سبد خرید</span>
         </button>
       </div>
     </div>
