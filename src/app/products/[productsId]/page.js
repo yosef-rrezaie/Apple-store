d7d96a09d92158e9416bf200643d44016fa2ab80
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-08-06 15:23:33 +0330

    filter by id

--- src/app/products/[productsId]/page.js
+++ src/app/products/[productsId]/page.js
@@ -8,8 +8,9 @@ export default async function Page({ params }) {
 
   try {
     await connectDB()
-    const ad = await Ad.findOne({ code: Number(productsId) }); 
-    if (!ad) return notFound(); 
+    const ad = await Ad.findOne({ _id: Object(productsId) }); 
+    console.log(ad)
+    // if (!ad) return notFound(); 
     return <ProductPage information={JSON.parse(JSON.stringify(ad))} />
   } catch (err) {
     console.error("DB error:", err)
