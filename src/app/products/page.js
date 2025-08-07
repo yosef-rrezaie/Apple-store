4a5244ca6d57d16e72fff87da343ea63afe6fd85
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-08-07 05:00:14 +0330

    solve some problems for product main page

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -6,11 +6,10 @@ async function page({ searchParams }) {
   const category = searchParams.category || "همه";
   const sort = searchParams.sort || "101";
   const search = searchParams.search || "";
-  console.log(category , sort , search);
   try {
     await connectDB();
     const filter = {};
-    if (category !== "all") {
+    if (category !== "همه") {
       filter.category = category;
     }
     if (search.trim !== "") {
@@ -18,17 +17,19 @@ async function page({ searchParams }) {
     }
 
     const sortOptions = {};
-    if (sort === "102") {
+    if (sort === "101") {
+      sortOptions.price = +1;
+    } else if (sort === "102") {
       sortOptions.price = -1;
-    } else if (sort === "101") {
-      sortOptions.price = -1;
-    }  else if (sort === "103") {
+    } else if (sort === "103") {
       sortOptions.createdAt = -1;
     }
+     else if (sort === "104") {
+      sortOptions.createdAt = +1;
+    }
 
-    const products = await Ad.find(filter).sort(sortOptions)
-    
-
+    const products = await Ad.find(filter).sort(sortOptions);
+    console.log(products);
   } catch (err) {
     console.log(err);
   }
--- src/components/modules/MainProducts.js
+++ src/components/modules/MainProducts.js
@@ -4,14 +4,17 @@ import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { FaStore } from "react-icons/fa";
+const sortOption = [ "همه","اپل واچ", "ایرپاد", "آیپد", "آیفون", "شارژر",  "هوم پاد", "اپل تی وی" ]
 
 function MainProducts() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [priceSort, setPriceSort] = useState(searchParams.get("sort") || 101);
-  const [categorySort, setCategorySort] = useState(searchParams.get("category") || "همه");
+  const [categorySort, setCategorySort] = useState(
+    searchParams.get("category") || "همه"
+  );
   const categoryHandler = (category) => {
-    setCategorySort(category)
+    setCategorySort(category);
     const params = new URLSearchParams(searchParams.toString());
     params.set("category", category);
     router.push(`?${params.toString()}`);
@@ -42,27 +45,20 @@ function MainProducts() {
             <option value="101">ارزان‌ترین</option>
             <option value="102">گران‌ترین</option>
             <option value="103">جدیدترین</option>
-            {/* <option value="104">پرفروش‌ترین</option> */}
+            <option value="104">قدیمی ترین</option>
           </select>
         </div>
 
         <div className="overflow-x-auto scrollbar-hide">
           <div className="flex gap-3 w-max">
-            {[
-              "همه",
-              "اپل واچ",
-              "ایرپاد",
-              "آیپد",
-              "آیفون",
-              "شارژر",
-              "هوم پاد",
-              "اپل تی وی",
-            ].map((category) => (
+            {sortOption.map((category) => (
               <button
                 key={category}
                 onClick={() => categoryHandler(category)}
                 className={`whitespace-nowrap border border-gray-300 px-3 py-1 md:px-4 lg:px-5 text-sm rounded-full
-                 hover:bg-orange-100 hover:border-orange-400 transition ${category===categorySort && "bg-primary text-white"}`}
+                 hover:bg-orange-100 hover:border-orange-400 transition ${
+                   category === categorySort && "bg-primary text-white"
+                 }`}
               >
                 {category}
               </button>
@@ -71,100 +67,7 @@ function MainProducts() {
         </div>
       </div>
       {/* products */}
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4">
-        <div className="bg-[#F6F6F6] rounded-[10px] p-4 shadow-sm hover:shadow-md transition">
-          <div className="flex justify-center">
-            <Image
-              src="/images/pc-laptop.png"
-              width={300}
-              height={200}
-              className="w-full max-h-40 object-contain"
-              alt="لپتاپ"
-            />
-          </div>
-          <p className="text-center font-semibold mt-2">لپتاب 13 اینچی ایسوز</p>
-
-          <div className="mt-2 border-t border-gray-200"></div>
-
-          <p className="text-center text-red-600 text-lg font-bold mt-3">
-            {sp(75000000)} تومان
-          </p>
-
-          <div className="mt-4 flex items-center">
-            <FaStore className="text-red-500 text-xl" />
-            <p className="mr-2 font-medium text-gray-700">
-              فروشگاه دیجیتال‌پلاس
-            </p>
-          </div>
-
-          <div className="mt-4 flex justify-end">
-            <button className="bg-orange-500 text-white px-3 py-1 rounded-md text-sm hover:bg-orange-600 transition">
-              افزودن به سبد خرید
-            </button>
-          </div>
-        </div>
-        <div className="bg-[#F6F6F6] rounded-[10px] p-4 shadow-sm hover:shadow-md transition">
-          <div className="flex justify-center">
-            <Image
-              src="/images/airpadmax.png"
-              width={300}
-              height={200}
-              className="w-full max-h-40 object-contain"
-              alt="لپتاپ"
-            />
-          </div>
-          <p className="text-center font-semibold mt-2">لپتاب 13 اینچی ایسوز</p>
-
-          <div className="mt-2 border-t border-gray-200"></div>
-
-          <p className="text-center text-red-600 text-lg font-bold mt-3">
-            {sp(75000000)} تومان
-          </p>
-
-          <div className="mt-4 flex items-center">
-            <FaStore className="text-red-500 text-xl" />
-            <p className="mr-2 font-medium text-gray-700">
-              فروشگاه دیجیتال‌پلاس
-            </p>
-          </div>
-
-          <div className="mt-4 flex justify-end">
-            <button className="bg-orange-500 text-white px-3 py-1 rounded-md text-sm hover:bg-orange-600 transition">
-              افزودن به سبد خرید
-            </button>
-          </div>
-        </div>
-        <div className="bg-[#F6F6F6] rounded-[10px] p-4 shadow-sm hover:shadow-md transition">
-          <div className="flex justify-center">
-            <Image
-              src="/images/pc-laptop.png"
-              width={300}
-              height={200}
-              className="w-full max-h-40 object-contain"
-              alt="لپتاپ"
-            />
-          </div>
-          <p className="text-center font-semibold mt-2">لپتاب 13 اینچی ایسوز</p>
-
-          <div className="mt-2 border-t border-gray-200"></div>
-
-          <p className="text-center text-red-600 text-lg font-bold mt-3">
-            {sp(75000000)} تومان
-          </p>
-
-          <div className="mt-4 flex items-center">
-            <FaStore className="text-red-500 text-xl" />
-            <p className="mr-2 font-medium text-gray-700">
-              فروشگاه دیجیتال‌پلاس
-            </p>
-          </div>
-
-          <div className="mt-4 flex justify-end">
-            <button className="bg-orange-500 text-white px-3 py-1 rounded-md text-sm hover:bg-orange-600 transition">
-              افزودن به سبد خرید
-            </button>
-          </div>
-        </div>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-8">
         <div className="bg-[#F6F6F6] rounded-[10px] p-4 shadow-sm hover:shadow-md transition">
           <div className="flex justify-center">
             <Image
