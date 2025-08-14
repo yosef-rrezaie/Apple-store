8203fbff26ba81c59983eeec127de46e85e96814
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-08-14 18:28:03 +0330

    logic searchbar side

--- src/components/layout/SearchBarPc.js
+++ src/components/layout/SearchBarPc.js
@@ -1,6 +1,17 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 function SearchBarPc() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [searchBar, setSearchBar] = useState("");
+  function clickHandler() {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("search", searchBar);
+    router.push(`/products?${params.toString()}`);
+  }
   return (
     <div className="hidden lg:flex gap-2">
       <button className="h-11 w-35 bg-primary rounded-full flex items-center justify-center gap-3">
@@ -17,12 +28,15 @@ function SearchBarPc() {
           name=""
           placeholder="جستجو کنید"
           className="w-full bg-[#FBFBFB] placeholder:text-[#282828B2] placeholder:font-light focus:outline-none"
+          value={searchBar}
+          onChange={(e) => setSearchBar(e.target.value)}
         />
         <Image
           src="/svg/search-normal.svg"
           width={20}
           height={20}
           alt="search"
+          onClick={clickHandler}
         />
       </div>
     </div>
